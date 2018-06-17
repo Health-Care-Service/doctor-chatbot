50a02b40e13fbe3d2ee62acd95abebfdf558a38a
Dawid Perdek <perdekdawid@gmail.com>
2018-06-17 23:46:21 +0200

    chat window done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  conversation = [];
+  text = '';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+  }
+
+  sendMessage() {
+    this.conversation.push({
+      type: 'send',
+      text: this.text
+    });
+
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json');
+
+    this.http.post('http://localhost:4201', JSON.stringify({text: this.text}), {headers: headers})
+      .subscribe(message => {
+        this.text = '';
+        this.conversation.push({
+          type: 'received',
+          text: message
+        });
+      });
+  }
+
+  userTextChanged(event) {
+    if (event.keyCode === 13) {
+      this.sendMessage();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NgxAutoScrollModule} from 'ngx-auto-scroll';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule,
+    HttpModule,
+    NgxAutoScrollModule,
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
